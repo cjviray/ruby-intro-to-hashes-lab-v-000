1bc6d04c0d1fc840d0f108cb61d02d01f0056f43
Christian V <viray.christianj@gmail.com>
2018-11-08 05:15:10 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -50,7 +50,7 @@ end
 def monopoly_with_fourth_tier
 	monopoly = {
 	  :railroads => {
-	   :pieces = 4,
+	   :pieces => 4,
 	   :names => {
 	    :reading_railroad{
 	      :mortgage_value => '$100'
