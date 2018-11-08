f17d19cd71fb05328c9bce46476530e36aed4ffd
Christian V <viray.christianj@gmail.com>
2018-11-08 04:25:34 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -68,7 +68,6 @@ def monopoly_with_fourth_tier
 	    :shortline => {
 	      :mortage_value => '$800'
 	    }
-	    }
 	    :rent_in_dollars => {
 	      :one_piece_owned => 25,
 	      :two_pieces_owned => 50, 
